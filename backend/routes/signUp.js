c871aa13391dba615c17d4562aa7bb5eff5b0c70
harshsaboo777 <106770712+harshsaboo777@users.noreply.github.com>
2023-08-21 11:56:27 +0530

    Delete backend directory

--- backend/routes/signUp.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createUser, logInUser } from "../controllers/signUp.controller.js";
-//const {createUser, logInUser} = require('../controllers/signUp.controller.js');
-const router = express.Router();
-
-router.post("/signUp", createUser);
-router.post("/login", logInUser);
-
-export default router;
